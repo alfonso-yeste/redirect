9f1e2c36d0d3feacef7b2c01c35915104d894b25
Alfonso Yeste <68972892+alfonso-yeste@users.noreply.github.com>
2021-03-09 07:39:43 +0100

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,9 @@
+function redireccionar() {
+    
+    if (Math.random() * 100 <= 70) {
+        destino = 'https://xb23d8i1.optimalworkshop.com/optimalsort/q747txv7';
+    } else {
+        destino = 'https://xb23d8i1.optimalworkshop.com/treejack/gamma';
+    }
+}
+setTimeout(redireccionar, 3000);
