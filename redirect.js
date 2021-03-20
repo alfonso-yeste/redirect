e46bb2bc5cd57dbb5ae413ec733127b65e85d6b0
Alfonso Yeste <68972892+alfonso-yeste@users.noreply.github.com>
2021-03-20 16:05:33 +0100

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,9 +1,9 @@
 function redireccionar() {
     
     if (Math.random() * 100 <= 70) {
-        destino = 'https://xb23d8i1.optimalworkshop.com/optimalsort/q747txv7';
+        destino = 'https://xb23d8i1.optimalworkshop.com/optimalsort/2u55318x-0';
     } else {
-        destino = 'https://xb23d8i1.optimalworkshop.com/treejack/gamma';
+        destino = 'https://xb23d8i1.optimalworkshop.com/treejack/310bfd7c-0';
     }
 }
 setTimeout(redireccionar, 3000);
