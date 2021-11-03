d41375af49092377ee0ff9d942964ec7bb0798db
Alfonso Yeste <68972892+alfonso-yeste@users.noreply.github.com>
2021-11-03 10:10:14 +0100

    Update redirect_v2.js

--- redirect_v2.js
+++ redirect_v2.js
@@ -1,1 +1,8 @@
 
+function redireccionar() {
+    
+    if (Math.random() * 100 <= 70) {
+        destino = 'https://xb23d8i1.optimalworkshop.com/treejack/w4m71em1';
+    } 
+}
+setTimeout(redireccionar, 100);
