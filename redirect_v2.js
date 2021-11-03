acab6234dfefdf2649bdee653e37a8b29472d439
Alfonso Yeste <68972892+alfonso-yeste@users.noreply.github.com>
2021-11-03 10:26:44 +0100

    Update redirect_v2.js

--- redirect_v2.js
+++ redirect_v2.js
@@ -1,8 +1,20 @@
-
-function redireccionar() {
-    
-    if (Math.random() * 100 <= 70) {
-        destino = 'https://xb23d8i1.optimalworkshop.com/treejack/w4m71em1';
-    } 
+	      
+      function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
-setTimeout(redireccionar, 100);
+      
+    function redireccionar() {
+      
+      var id = getParameterByName('ID');
+      
+    if (Math.random() * 100 <= 80) {
+        destino = 'https://xb23d8i1.optimalworkshop.com/treejack/w4m71em1?ID=' + id;
+    } 
+    window.location= destino;
+}
+setTimeout(redireccionar,100);
